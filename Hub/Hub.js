342d8cedd8852db73281adbeef2a9c47a60448f0
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-11-16 22:28:48 +0200

    finish lab-13

--- Hub/Hub.js
+++ Hub/Hub.js
@@ -1,31 +1,75 @@
-'use strict';
+"use strict";
 
-const PORT = process.env.PORT || 3000;
-const hub = require('socket.io')(PORT);
-const capsSystem = hub.of("/caps");
-
+require("dotenv").config();
 
 const pickup = require("../Vendor/pickup");
 const inTransit = require("../Driver/inTransit");
 const delivered = require("../Driver/delivered");
 
+const PORT = process.env.PORT || 3000;
+
+const hub = require("socket.io")(PORT);
+
+const capsSystem = hub.of("/caps");
+
+const messageQueue = {
+  pickup: {},
+  delivered: {},
+};
 
 capsSystem.on("connection", (socket) => {
-    console.log("CAPS SYSTEM CONNECTING", socket.id);
+  console.log("CAPS SYSTEM CONNECTING", socket.id);
+  console.log("=====================================");
+  socket.on("pickup", (payload) => {
+    console.log("Vendor sent to Hub new order");
+    console.log("=====================================");
+    messageQueue.pickup[payload.orderId] = payload;
+    pickup(payload);
+    console.log("=====================================");
+    // socket.join(payload.store);
+    capsSystem.emit("pickup", payload);
+  });
 
-    socket.on("pickup", (payload) => {
-        pickup(payload);
-       
-        capsSystem.emit("pickup", payload);
-    });
+  socket.on("received", (payload) => {
+    console.log(
+      `the ${payload.moduleName} sent for the Hub to delete the order ${payload.payload.orderId}`
+    );
+    console.log("=====================================");
+    if (payload.event === "pickup") {
+      delete messageQueue.pickup[payload.payload.orderId];
+    }
+    if (payload.event === "delivered") {
+      delete messageQueue.delivered[payload.payload.orderId];
+    }
+    console.log(messageQueue);
+    console.log("=====================================");
+  });
 
-    socket.on("in-transit", (payload) => {
-        inTransit(payload);
-       
-    });
+  socket.on("in-transit", (payload) => {
+    console.log("The driver sent to the Hub that the ordrer is on the way");
+    console.log("=====================================");
+    inTransit(payload);
+    console.log("=====================================");
+    capsSystem.emit("delivered", payload);
+  });
 
-    socket.on("delivered", (payload) => {
-        delivered(payload);
-        capsSystem.emit("delivered", payload);
-    });
+  socket.on("delivered", (payload) => {
+    delivered(payload);
+  });
+
+  socket.on("get_all", (payload) => {
+    console.log(`get all of the missing ${payload.event} messages`);
+    console.log("=====================================");
+
+    if (payload.event === "pickup") {
+      Object.keys(messageQueue.pickup).forEach((orderId) => {
+        socket.emit("pickup", messageQueue.pickup[orderId]);
+      });
+    }
+    if (payload.event === "delivered") {
+      Object.keys(messageQueue.delivered).forEach((orderId) => {
+        socket.emit("delivered", messageQueue.delivered[orderId]);
+      });
+    }
+  });
 });
